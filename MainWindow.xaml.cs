238b3388fa77a893ba8c3ffaaf5e9d5aefa71582
Gitii <Gitii@users.noreply.github.com>
2022-07-24 15:21:51 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.Web.WebView2.Core;
+
+namespace WindowsAppSdkHostWithBrowser;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+
+    private void myButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            Uri targetUri = new Uri(addressBar.Text);
+            MyWebView.Source = targetUri;
+        }
+        catch (FormatException ex)
+        {
+            // Incorrect address entered.
+        }
+    }
+
+    private void MyWebView_OnNavigationCompleted(WebView2 sender, CoreWebView2NavigationCompletedEventArgs args)
+    {
+        addressBar.Text = sender.Source.ToString();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿#define V3
+
+using System;
+using System.Threading.Tasks;
+using CommunityToolkit.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WindowsAppSdkHostWithBrowser;
+
+public static partial class Program
+{
+#if V1
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        var builder = new WindowsAppSdkHostBuilder<App>();
+
+        builder.ConfigureServices(
+            (_, collection) =>
+            {
+                // If your main Window is named differently, change it here.
+                collection.AddSingleton<MainWindow>();
+            }
+        );
+
+        var app = builder.Build();
+
+        app.StartAsync().GetAwaiter().GetResult();
+    }
+#elif V2
+    [STAThread]
+    public async static Task Main(string[] args) // <- only change *async Task*
+    {
+        var builder = new WindowsAppSdkHostBuilder<App>();
+
+        builder.ConfigureServices(
+            (_, collection) =>
+            {
+                // If your main Window is named differently, change it here.
+                collection.AddSingleton<MainWindow>();
+            }
+        );
+
+        var app = builder.Build();
+
+        app.StartAsync().GetAwaiter().GetResult();
+    }
+#elif V3
+    [STAThread]
+    public async static Task Main(string[] args) // <- this like in V2 and ...
+    {
+        var builder = new WindowsAppSdkHostBuilder<App>();
+
+        builder.ConfigureServices(
+            (_, collection) =>
+            {
+                // If your main Window is named differently, change it here.
+                collection.AddSingleton<MainWindow>();
+            }
+        );
+
+        var app = builder.Build();
+
+        await app.StartAsync(); // <- ... that
+    }
+#endif
+}
